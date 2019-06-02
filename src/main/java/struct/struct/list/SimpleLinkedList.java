5cb6d1733037662f02ec4ae45af77661ce5622d9
psk <psk3060@naver.com>
2019-06-02 21:04:59 +0900

    Print 및 Insert Idx

--- src/main/java/struct/struct/list/SimpleLinkedList.java
+++ src/main/java/struct/struct/list/SimpleLinkedList.java
@@ -4,6 +4,8 @@ import struct.struct.common.Node;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+import java.util.*;
+
 public class SimpleLinkedList {
     private Node head;
 
@@ -58,13 +60,52 @@ public class SimpleLinkedList {
      * Head의 Next가 null일 경우 별 상관없음, 만약 있다면 새로운 노드의 Next를 head의 Next로
      */
     public void insertNodeFirst(Node newNode) {
-        if( head.getNext() == null ) {
-            
-        } else {
-            newNode.setNext(head.getNext());
-        }
+        lock.lock();
 
-        head.setNext(newNode);
+        try {
+            if( head.getNext() == null ) {
+            
+            } else {
+                newNode.setNext(head.getNext());
+            }
+    
+            head.setNext(newNode);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * @param idx : newNode의 위치. 기존 idx 번째 노드의 경우 newNode의 Next로
+     * @param newNode : idx번째 들어갈 노드
+     */
+    public void insertNodeIdx(int idx, Node newNode) {
+        // 첫 번째 데이터 삽입(idx를 0 이하 입력했을 경우)
+        if( idx <= 0 ) {
+            insertNodeFirst(newNode);
+        } else {
+            // 가장 마지막에 삽입 idx가 크기보다 더 크거나 같게 입력되었을 때
+            if( idx >= count() ) {
+                insertNode(newNode);
+            } else {
+                // 이전 노드 가지고 옴
+                Node preNode = getNode(idx - 1);
+
+                // 원래 노드(새로운 노드의 Next)
+
+                try {
+                    // 원래 있었던 노드 가지고 옴
+                    Node originalNode = getNode(idx);
+                    // 새로운 노드의 다음을 원래 노드로
+                    newNode.setNext(originalNode);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+
+                // 이전 노드의 다음 노드를 새로운 노드로
+                preNode.setNext(newNode);
+            }
+        }
     }
 
     /**
@@ -120,7 +161,6 @@ public class SimpleLinkedList {
             } finally {
                 lock.unlock();
             }
-
         } 
     }
 
@@ -130,17 +170,25 @@ public class SimpleLinkedList {
     public void deleteLast() {
         int count = this.count();
         if( count > 0) {
-            Node target = null;
+            lock.lock();
             try {
-                target = getNode(count - 1);
-            } catch(Exception e) {
-                target = head;
-            }
+                Node target = null;
+                try {
+                    target = getNode(count - 1);
+                } catch(Exception e) {
+                    target = head;
+                }
 
-            target.setNext(null);
+                target.setNext(null);
+            } finally {
+                lock.unlock();
+            }
         }
     }
 
+    /**
+     * idx 번째 데이터 삭제
+     */
     public void deleteIdx(int idx) {
         int count = this.count();
 
@@ -148,16 +196,39 @@ public class SimpleLinkedList {
             if( idx == 0 ) {
                 System.out.println("삭제 인덱스는 1번부터 시작입니다.");
             } else {
-                Node delNode = getNode(idx);
-                if( delNode != null ) {
-                    // 0이나 -일 경우 head를 가지고옴
-                    Node preNode = getNode(idx - 1);
+                lock.lock();
+                try {
+                    Node delNode = getNode(idx);
+                    if( delNode != null ) {
+                        // 0이나 -일 경우 head를 가지고옴
+                        Node preNode = getNode(idx - 1);
 
-                    // 이전 노드의 다음 노드 조정(null로 설정할 수도 있음)
-                    preNode.setNext(delNode.getNext());
+                        // 이전 노드의 다음 노드 조정(null로 설정할 수도 있음)
+                        preNode.setNext(delNode.getNext());
+                    }
+                } finally {
+                    lock.unlock();
                 }
             }
         }
     }
 
+    /**
+     * 앞에서부터 읽어서 List에 담은 후 반환
+     */
+    public List<Node> frontPrintList() {
+        List<Node> result = new ArrayList<Node>();
+
+        if( count() > 0 ) {
+            Node target = head.getNext();
+
+            while(target != null) {
+                result.add(target);
+
+                target = target.getNext();
+            }
+        } 
+
+        return result;
+    }
 }
